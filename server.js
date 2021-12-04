dd3c4526dbd6658d33832363e39926393104055b
Eliud Heredia <eliudheredia@Eliuds-MBP.fios-router.home>
2021-12-04 17:29:41 -0500

    node

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
+}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/project');
+
+require('./services/user-service')(app);
+
+app.listen(process.env.PORT || 4000);
