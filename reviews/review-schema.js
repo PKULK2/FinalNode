9b43e2d4bce129bbe77b43b7ae9a1c65dde69470
tagreizer <greizer.t@northeastern.edu>
2021-12-14 17:40:20 -0500

    added movie title to review

--- reviews/review-schema.js
+++ reviews/review-schema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
                                        imdbID: String,
+                                        MovieName: String,
                                         user: String,
                                         date: String,
                                         content: String,
